6c394ddf71485e5f2f7e610d98f0e3c05888f91a
Alex Bowden <abowden@spotx.tv>
2019-05-17 00:10:33 -0600

    FoolInterview: added view tests

--- project/foolinterview/main/tests.py
+++ project/foolinterview/main/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+class ViewsTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_index(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main/home.html')
+
+    def test_article(self):
+        url = reverse('article', kwargs={'uuid':'a7acd8c8-c5ce-11e7-9fa6-0050569d4be0'})
+        print(url)
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main/article.html')
